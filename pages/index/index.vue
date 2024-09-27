babf0a18009db3ef6e3e81f28547fcfe35390f7b
waset <waset@foxmail.com>
2024-09-27 15:59:34 +0800

    ci(github): build action

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,23 +2,13 @@
   <div>
     <div class="container">
       <div class="bg-white text-black dark:bg-black dark:text-white">
-        <h1
-          class="py-10 text-center"
-          @click="toggleDark()"
-        >
+        <h1 class="py-10 text-center" @click="toggleDark()">
           <span class=" ">Hello Word !</span>
         </h1>
-
-        <van-button
-          type="primary"
-          @click="showToast('toast')"
-        >
+        <van-button type="primary" @click="showToast('toast')">
           button
         </van-button>
-        <VanButton
-          type="success"
-          @click="showNotify('notify')"
-        >
+        <VanButton type="success" @click="showNotify('notify')">
           button
         </VanButton>
         <LazyVanButton type="default">
--- uno.config.ts
+++ uno.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   ],
   content: {
     pipeline: {
-      include: [
-        '**/*.{vue,ts}',
-      ],
+      include: ['**/*.{vue,ts}'],
     },
   },
   shortcuts: {
