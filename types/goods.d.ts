14cf22563b18ff1917fe8e9378149b3dd4da16f8
LdddLi <1653243099@qq.com>
2024-09-30 09:28:51 +0800

    feat: 根据需求文档、定义成品相关字段

--- types/goods.d.ts
+++ types/goods.d.ts
@@ -1,3 +1,4 @@
+// 成品列表
 interface FinishedProduct {
   name: string
   image: string
@@ -5,12 +6,94 @@ interface FinishedProduct {
   info: FinishedProductInfo[]
 }
 
+enum Status {
+  /**
+   * 调拨在途
+   */
+  OnWay = 1,
+  /**
+   * 在库
+   */
+  InStock = 2,
+  /**
+   * 维修中
+   */
+  Repairing = 3,
+}
+
 interface FinishedProductInfo {
   name: string
   description: string
 }
 
-enum Status {
-  OnWay = 1,
-  Other = 2,
+// 仓库入库单
+interface WarehouseReceipt {
+  /**
+   * 操作员
+   */
+  operator: string
+  /**
+   * 入库单号
+   */
+  number: string
+  /**
+   * 入库类型
+   */
+  type: WarehouseReceiptType
+  /**
+   * 入库单状态
+   */
+  status: WarehouseReceiptStatus
+  /**
+   * 总件数
+   */
+  totalCount: number
+  /**
+   * 总标价
+   */
+  totalPrice: number
+  /**
+   * 总金重
+   */
+  totalWeight: number
+  /**
+   * 总入网费
+   */
+  NetworkAccessFee: number
+  /**
+   * 备注
+   */
+  remark: string
+  /**
+   * 创建时间
+   */
+  createTime: string
+  /**
+   * 完成时间
+   */
+  completeTime: string
+}
+
+// 入库类型
+enum WarehouseReceiptType {
+  /**
+   * 手动入库
+   */
+  Manual = 1,
+}
+
+// 入库单状态
+enum WarehouseReceiptStatus {
+  /**
+   * 已完成
+   */
+  Completed = 1,
+  /**
+   * 已撤销
+   */
+  Cancelled = 2,
+  /**
+   * 草稿
+   */
+  Draft = 3,
 }
