6f34e952528cf7b1b3a65f6412b6809b78e2fbec
XieJingao <496929151@qq.com>
2024-09-30 17:13:15 +0800

    feat: 新增底部按钮、折叠面板、输入框组件

---
+++ components/bottomBtn.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  cancelText?: string
+  confirmText?: string
+  centerText?: string
+}>(), {
+  cancelText: '取消',
+  confirmText: '确认',
+  centerText: '中间按钮',
+})
+</script>
+
+<template>
+  <div class="btn grid-12 offset-2">
+    <button class="btn-left col-span-4 offset-1 rgba(255, 255, 255, 0.2)">
+      {{ props.cancelText }}
+    </button>
+    <button class="btn-right col-span-6">
+      {{ props.confirmText }}
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  --uno: 'fixed bottom-0 left-0 right-0 bg-[#f1f5fe] p-[12px_16px] text-[16px] font-bold';
+  &-left {
+    background: #ffffff;
+    box-shadow: rgb(110, 166, 255, 0.3) 0px 6px 6px;
+    --uno: 'text-[16px] py-[8px] border-none text-center rounded-[36px] mr-[8px]';
+  }
+
+  &-right {
+    background: linear-gradient(to bottom, #1a6beb, #6ea6ff);
+    box-shadow: rgb(110, 166, 255, 0.3) 0px 6px 6px;
+    --uno: 'text-[16px] py-[8px] border-none flex-1 rounded-[36px] ml-[8px] text-[#FFFFFF]';
+  }
+}
+</style>
