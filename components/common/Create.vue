7cd1be2cd3efb24afdae39ff53633967fe7742b6
sampson762 <1769301207@qq.com>
2024-09-30 15:15:05 +0800

    style: class 写到 scss里

--- components/common/Create.vue
+++ components/common/Create.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <div class="pos-fixed bottom-[77px] right-[14px] wh-[56px] bg-gradient-linear-[40deg,#236CFA,#0085FFC3,#59C6E5] rounded-full flex justify-center items-center">
+    <div class="createbox">
       <div
-        class="pos-fixed wh-[52px] bg-gradient-linear-[40deg,#005BFF,#0085FFC3,#49BEE6] rounded-full flex justify-center items-center pos-relative">
-        <div class="w-[20px] h-[4px] rounded-full bg-gradient-linear-[40deg,#FFFFFF,#BCCEFF]" />
+        class="create2">
+        <div class="row" />
         <div
-          class="w-[20px] h-[4px] rounded-full bg-gradient-linear-[40deg,#FFFFFF,#BCCEFF] pos-absolute rotate-90" />
+          class="cols" />
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .createbox {
+  --uno: 'pos-fixed bottom-[77px] right-[14px] wh-[56px] bg-gradient-linear-[40deg,#236CFA,#0085FFC3,#59C6E5] rounded-full flex justify-center items-center';
+}
+.create2 {
+  --uno: 'pos-fixed wh-[52px] bg-gradient-linear-[40deg,#005BFF,#0085FFC3,#49BEE6] rounded-full flex justify-center items-center pos-relative';
+}
+.row {
+  --uno: 'w-[20px] h-[4px] rounded-full bg-gradient-linear-[40deg,#FFFFFF,#BCCEFF]';
+}
+.cols {
+  --uno: 'w-[20px] h-[4px] rounded-full bg-gradient-linear-[40deg,#FFFFFF,#BCCEFF] pos-absolute rotate-90';
+}
+</style>
