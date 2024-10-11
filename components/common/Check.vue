3f0ea52221c68039af183ca2884bea7c4faef2ef
LdddLi <1653243099@qq.com>
2024-10-11 17:45:25 +0800

    feat: 新增复选框组件、会员等级徽章组件

---
+++ components/common/Check.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// 勾选框
+const props = defineProps<{
+  checked: boolean
+}>()
+</script>
+
+<template>
+  <div class="check-box">
+    <template v-if="props.checked">
+      <div class="flex justify-center items-center rounded-full w-4 h-4 border-solid border-2 border-#3971F3 bg-#fff">
+        <nuxt-img src="/icons/check_box.svg" class="w-full h-full text-#3971F3" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="rounded-full w-4 h-4 border-solid border-2 border-#CBCDD1" />
+    </template>
+  </div>
+</template>
