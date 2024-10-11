0a7ef07453b029baae432dfaee698985d62469bb
sampson762 <1769301207@qq.com>
2024-10-11 10:11:03 +0800

    style: 黑夜模式开关样式调整

--- components/common/Dark.vue
+++ components/common/Dark.vue
@@ -8,23 +8,23 @@ const toggleDark = useToggle(isDark)
       <div class="p-[4px] rounded-full flex-center-row light:bg-[#FFF] dark:bg-transparent">
         <template v-if="isDark">
           <NuxtImg src="/dark/sun-line.svg" width="16" />
-          <span class="text-size-[12px] ml-[4px] color-[#fff]">浅色</span>
+          <span class="text-size-[12px] line-height-[24px] ml-[4px] color-[#fff]">浅色</span>
         </template>
         <template v-else>
           <NuxtImg src="/dark/sun-blue.svg" width="16" />
-          <span class="text-size-[12px]  ml-[4px] color-[#3971F3]">浅色</span>
+          <span class="text-size-[12px] line-height-[24px]  ml-[4px] color-[#3971F3]">浅色</span>
         </template>
       </div>
       <div class="p-[4px] rounded-full flex-center-row light:bg-transparent dark:bg-[#FFF]">
         <template v-if="isDark">
           <NuxtImg src="/dark/moon-fill.svg" width="16"
           />
-          <span class="text-size-[12px]  ml-[4px] color-[#3971F3]">深色</span>
+          <span class="text-size-[12px]  line-height-[24px] ml-[4px] color-[#3971F3]">深色</span>
         </template>
         <template v-else>
           <NuxtImg src="/dark/moon-white.svg" width="16"
           />
-          <span class="text-size-[12px]  ml-[4px] color-[#fFf]">深色</span>
+          <span class="text-size-[12px]  line-height-[24px] ml-[4px] color-[#fFf]">深色</span>
         </template>
       </div>
     </div>
