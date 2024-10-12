ec0191eaea9c91478cbcdd4c5bac3a4023c6ea45
sampson762 <1769301207@qq.com>
2024-10-12 14:50:41 +0800

    feat: 开发个人中心页面

---
+++ pages/my/user/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="px-[16px] py-[24px]">
+      <my-user-userinfo />
+    </div>
+    <div class="px-[16px] ">
+      <my-user-option />
+    </div>
+
+    <common-tabbar />
+  </div>
+</template>
