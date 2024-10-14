20b607cbabb60616372f13f153d74cbe46166308
sampson762 <1769301207@qq.com>
2024-10-14 16:33:00 +0800

    refactor: 使用colorMode  替代 usedark

--- components/common/Dark.vue
+++ components/common/Dark.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts">
-const toggleDark = useToggle(isDark)
+const colorMode = useColorMode()
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
+}
 </script>
 
 <template>
-  <div class=" w-fit" @click="toggleDark()">
+  <div class=" w-fit" @click="toggleColorMode()">
     <div class="bg-[#FFFFFF66] rounded-full p-[2px] flex-center-row">
       <div class="p-[4px] rounded-full flex-center-row light:bg-[#FFF] dark:bg-transparent">
-        <template v-if="isDark">
+        <template v-if="colorMode.preference === 'dark'">
           <icon name="jdy:sun-line" size="16" />
-          <span class="text-size-[12px] line-height-[24px] ml-[4px] color-[#fff]">浅色</span>
         </template>
         <template v-else>
           <icon name="jdy:sun-blue" size="16" />
-          <span class="text-size-[12px] line-height-[24px]  ml-[4px] color-[#3971F3]">浅色</span>
         </template>
+        <span class="text-size-[12px] line-height-[24px] ml-[4px] color-[#3971F3] dark:color-[#fff]">浅色</span>
       </div>
       <div class="p-[4px] rounded-full flex-center-row light:bg-transparent dark:bg-[#FFF]">
-        <template v-if="isDark">
+        <template v-if="colorMode.preference === 'dark'">
           <icon name="jdy:moon-fill" size="16" />
-          <span class="text-size-[12px]  line-height-[24px] ml-[4px] color-[#3971F3]">深色</span>
         </template>
         <template v-else>
           <icon name="jdy:moon-white" size="16" />
-          <span class="text-size-[12px]  line-height-[24px] ml-[4px] color-[#fFf]">深色</span>
         </template>
+        <span class="text-size-[12px]  line-height-[24px] ml-[4px] color-[#fFf] dark:color-[#3971F3]">深色</span>
       </div>
     </div>
   </div>
