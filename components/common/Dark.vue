fc27b4c2cf6dbba6257264e5fe1d7b9884ee92a6
sampson762 <1769301207@qq.com>
2024-10-15 09:42:36 +0800

    refactor: 修改夜间模式

--- components/common/Dark.vue
+++ components/common/Dark.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
-
-const toggleColorMode = () => {
-  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
-}
+const { $colorMode } = useNuxtApp()
+const mode = computed({
+  get() {
+    return $colorMode.preference
+  },
+  set(newValue) {
+    $colorMode.preference = newValue
+  },
+})
 </script>
 
 <template>
-  <div class=" w-fit" @click="toggleColorMode()">
-    <div class="bg-[#FFFFFF66] rounded-full p-[2px] flex-center-row">
+  <div class=" w-fit select-none">
+    <div class="bg-[#FFFFFF66] rounded-full p-[2px] flex-center-row " @click="mode = mode === 'dark' ? 'light' : 'dark'">
       <div class="p-[4px] rounded-full flex-center-row light:bg-[#FFF] dark:bg-transparent">
-        <template v-if="colorMode.preference === 'dark'">
+        <template v-if="mode === 'dark'">
           <icon name="jdy:sun-line" size="16" />
         </template>
         <template v-else>
@@ -19,7 +23,7 @@ const toggleColorMode = () => {
         <span class="text-size-[12px] line-height-[24px] ml-[4px] color-[#3971F3] dark:color-[#fff]">浅色</span>
       </div>
       <div class="p-[4px] rounded-full flex-center-row light:bg-transparent dark:bg-[#FFF]">
-        <template v-if="colorMode.preference === 'dark'">
+        <template v-if="mode === 'dark'">
           <icon name="jdy:moon-fill" size="16" />
         </template>
         <template v-else>
