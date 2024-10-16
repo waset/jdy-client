cfc72720c666fba5f8c8d45f161251b5a779e8d0
sampson762 <1769301207@qq.com>
2024-10-16 14:18:29 +0800

    feat: 开发会员卡页面

--- pages/customer/member/card.vue
+++ pages/customer/member/card.vue
@@ -20,21 +20,33 @@ const checked = ref<boolean>(true)
         <div class="w-[100px] h-[66px] bg-[#FFFFFF66] rounded-[8px] mr-[20px] shrink-0" />
       </div>
 
-      <div class="flex-start">
+      <div class="flex-start mb-[12px]">
         <span class="w-[100px]">启用状态</span> <van-switch v-model="checked" size="24" />
       </div>
-      <div class="flex-start">
+      <div class="flex-start mb-[12px]">
         <span class="w-[100px]">会员卡主标题</span> <span>郑州金美福珠宝有限公司</span>
       </div>
-      <div class="flex-start">
-        <span class="w-[100px]">会员卡名称</span> <van-switch v-model="checked" size="24" />
+      <div class="flex-start mb-[12px]">
+        <span class="w-[100px]">会员卡名称</span>
+        <div class="bg-[#fff] flex-1  rounded-[24px] border-color-[#E6E6E8] border-solid border py-[6px] px-[12px]" :style="{ background: checked ? '#fff' : '#DEDEDE' }">
+          <input :disabled="!checked" type="text" class="w-full border-none bg-transparent" placeholder="请输入">
+        </div>
       </div>
-      <div class="flex-start">
-        <span class="w-[100px]">会员卡描述</span> <van-switch v-model="checked" size="24" />
+      <div class="flex-start mb-[12px]">
+        <span class="w-[100px]">会员卡描述</span>
+        <div class="bg-[#DEDEDE] flex-1 h-[110px] p-[12px] rounded-[8px] border-color-[#E9EBEE] border-solid border" :style="{ background: checked ? '#fff' : '#DEDEDE' }">
+          <textarea :disabled="!checked" class="w-full h-full resize-none border-none  bg-transparent" placeholder="默认提示文字" />
+        </div>
       </div>
 
-      <div class="flex-start">
-        <span class="w-[100px]">启用状态</span> <van-switch v-model="checked" size="24" />
+      <div class="flex-start mb-[8px]">
+        <span class="w-[100px]">会员logo</span>
+        <van-switch v-model="checked" size="24" />
+      </div>
+
+      <div class="flex-start text-[12px] color-[#808089]">
+        <span class="w-[100px]" />
+        <div>默认logo 不可修改</div>
       </div>
     </div>
 
