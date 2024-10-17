c743ae4f28402d444e1eb935e7db64724524272e
sampson762 <1769301207@qq.com>
2024-10-17 17:45:44 +0800

    refactor: 修改提示

--- components/work/card/greet.vue
+++ components/work/card/greet.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+const getGreeting = () => {
+  const hour = new Date().getHours()
+  if (hour < 12)
+    return '早上好'
+  if (hour < 18)
+    return '下午好'
+  return '晚上好'
+}
+</script>
+
 <template>
   <div class="mb-[8px]">
     <div class="text-size-xl line-height-[24px] font-semibold color-[#fff]">
-      早上好,孙暴风
+      {{ getGreeting() }},孙暴风
     </div>
     <div class="text-size-[14px] line-height-[24px] color-[#fff] mt-[8px]">
       {{ getDayAndWeek() }}
