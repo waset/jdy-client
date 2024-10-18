331e1f51ab0dbf83aa82dc0bfb522e4228c612c5
LdddLi <1653243099@qq.com>
2024-10-18 17:20:34 +0800

    refactor: 对照设计稿、调整销售明细页面

---
+++ components/sale/order/Nesting.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+}>()
+
+const folded = ref(true)
+
+const toggleFold = () => {
+  folded.value = !folded.value
+}
+</script>
+
+<template>
+  <div class="card">
+    <!-- header -->
+    <div class="flex-center-between px-[16px] py-[8px] bg-[#F1F5FE]">
+      <div class="flex-center-row gap-[8px]">
+        <slot name="tag" />
+        <div class="text-[14px] font-semibold color-[#1B2129]">
+          {{ props.title }}
+        </div>
+      </div>
+      <div class="font-size-[10px] color-[#333]" @click="toggleFold">
+        <van-icon :name="!folded ? 'arrow' : 'arrow-down'" size="12" />
+      </div>
+    </div>
+    <!-- info -->
+    <template v-if="folded">
+      <slot name="info" />
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  --uno: 'flex flex-col bg-[#fff] border-solid border-1 border-[#EFF0F6] rounded-[24px] shadow-[0_10px_20px_-7px_rgb(0,0,0,0.1)] gap-[12px] overflow-hidden';
+}
+</style>
