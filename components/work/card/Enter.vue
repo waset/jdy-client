908c019c7ca5016de1f335d9a711f5ae87e9713f
sampson762 <1769301207@qq.com>
2024-10-18 16:25:42 +0800

    refactor: 修改pr  key

--- components/work/card/Enter.vue
+++ components/work/card/Enter.vue
@@ -6,13 +6,13 @@ const props = defineProps<{
 
 <template>
   <div>
-    <template v-for="(item, index) in props.content" :key="index">
+    <template v-for="(item, index) in props.content" :key="`${item.title}-${index}`">
       <div>
         <div class="text-size-[14px] line-height-[20px] color-[#333] pb-[8px] border-b-[1px] b-b-solid border-b-[#E6E6E8] mb-[16px]">
           {{ item.title }}
         </div>
         <div class="grid-12 gap-row-[16px] gap-col-[24px]">
-          <template v-for="(child, i) in item.enter" :key="i">
+          <template v-for="(child, i) in item.enter" :key="`${child.name}-${i}`">
             <nuxt-link class="flex-start col-6 color-[#000]" :to="child.path">
               <div class="wh-[30px] bg-[#FF624C] rounded-full" />
               <div class="py-[6px] ml-[4px] text-size-[12px] line-height-[20px]">
