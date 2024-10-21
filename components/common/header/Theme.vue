854adbe74125c1eda7320daa59afa021a9e92a9a
LdddLi <1653243099@qq.com>
2024-10-21 10:11:01 +0800

    chore: 调整成品列表页、主题色渐变栏组件的样式

--- components/common/header/Theme.vue
+++ components/common/header/Theme.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="flex-center-between shrink-0 px-[16px] py-[8px] rounded-t-[24px] bg-[linear-gradient(90deg,#477ef500,#82d0faf2)]">
+  <div class="flex-center-between shrink-0 px-[16px] py-[8px] bg-[linear-gradient(90deg,#477ef500,#82d0faf2)]">
     <div class="flex-center-between gap-2 flex-1">
       <slot name="avatar" />
       <div class="text-[14px] font-semibold dark:color-[#fff]" :style="{ color: props.color ? props.color : '#1B2129' }">
--- components/product/finished/List.vue
+++ components/product/finished/List.vue
@@ -12,16 +12,16 @@ const goodsStatus = {
 </script>
 
 <template>
-  <div class="grid-12">
-    <div class="w-full bg-white blur-bga rounded-2xl border-solid border-[#EFF0F6] col-12" uno-lg="col-8 offset-2" uno-md="col-12">
-      <div class="rounded-6">
+  <div class="grid-12 px-[16px]">
+    <div class="w-auto bg-white blur-bga rounded-[24px] border-solid border-[#EFF0F6] col-12 overflow-hidden" uno-lg="col-8 offset-2" uno-md="col-12">
+      <div class="rounded-[24px]">
         <common-header-theme :title="props.productList.name">
           <common-tags type="orange" :text="goodsStatus[props.productList.status]" />
         </common-header-theme>
         <div class="flex flex-col gap-3 px-4 py-3">
           <template v-if="props.productList.info.length">
             <template v-for="(item, index) in props.productList.info" :key="index">
-              <div class="flex-center-between text-sm font-normal">
+              <div class="flex-center-between text-sm font-normal even:bg-[#F5F5F5]">
                 <div class="color-[#666666]">
                   {{ item.name }}
                 </div>
