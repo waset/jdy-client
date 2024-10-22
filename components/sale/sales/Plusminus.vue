e1701d0bc30e52f31a946fa8f209895dddebd0a6
LdddLi <1653243099@qq.com>
2024-10-22 16:31:46 +0800

    feat: 新增会员搜索页、商品搜索页及相关组件; 移除不再使用的页面

---
+++ components/sale/sales/Plusminus.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+// "+"符号按钮
+const props = withDefaults(defineProps<{
+  /**
+   * 尺寸
+   */
+  size?: number
+  /**
+   * 是否是加号
+   * true: 加符号 ; false: 减符号
+   */
+  isAdd?: boolean
+}>(), {
+  size: 32,
+  isAdd: true,
+})
+
+const emit = defineEmits(['buttonClick'])
+
+const handleClick = () => {
+  emit('buttonClick')
+}
+</script>
+
+<template>
+  <div class="bg-[#F1F5FE] rounded-[60px] flex justify-center items-center" :style="`width: ${props.size}px; height: ${props.size}px;`" @click="handleClick()">
+    <!-- <van-icon name="plus" color="#3971F3" :size="20" /> -->
+    <template v-if="props.isAdd">
+      <icon name="jdy:add" size="20" class="color-[#3971F3]" />
+    </template>
+    <template v-else>
+      <icon name="jdy:subtract" size="20" class="color-[#3971F3]" />
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
