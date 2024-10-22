5a813ee1de7405cc07a5e8065261c8b2e7df3f6e
LdddLi <1653243099@qq.com>
2024-10-22 17:46:01 +0800

    refactor: 完善新增销售单页 ; 新增支付方式的添加、删除方法

--- components/sale/add/Settlement.vue
+++ components/sale/add/Settlement.vue
@@ -2,70 +2,106 @@
 const props = defineProps<{
   goodinfo: any[]
 }>()
-const showPopover = ref(false)
+
 const actions = [
   { text: '微信' },
   { text: '支付宝' },
   { text: '现金' },
 ]
+const getInitialItems = () => [{ id: 1, isPopoverVisible: false, actions }]
+const items = ref(getInitialItems())
+let id = items.value.length + 1
+
+const insertItem = () => {
+  // 添加新的支付信息栏并初始化状态
+  items.value.push({ id: id++, isPopoverVisible: false, actions })
+}
+
+function removeItem(item: { id: number }) {
+  const i = items.value.findIndex(i => i.id === item.id)
+  if (i > -1) {
+    items.value.splice(i, 1)
+  }
+}
 </script>
 
 <template>
-  <div class="pb-[16px]">
+  <div class="px-[16px]">
     <common-fold title="结算信息">
       <div class="p-[16px]">
-        <div class="md:grid grid-cols-[1fr_1fr] gap-[16px] lg:grid-cols-[1fr_1fr_1fr]">
-          <template v-for="(item, index) in props.goodinfo" :key="index">
-            <div class="flex items-center justify-between pb-[8px]">
-              <div class="dark:text-[#FFFFFF] text-[16px] font-semibold">
-                {{ item.name }}
+        <div class="grid-cols-[1fr_1fr] gap-[16px]" uno-lg="grid-cols-[1fr_1fr_1fr]" uno-md="grid">
+          <div class="flex flex-col gap-[12px]">
+            <template v-for="(item, index) in props.goodinfo" :key="index">
+              <div class="flex items-center justify-between">
+                <div class="color-[#333] dark:color-[#FFFFFF] text-[16px] font-medium">
+                  {{ item.name }}
+                </div>
+                <common-frame
+                  v-model="item.num"
+                />
               </div>
-              <common-frame
-                v-model="item.num"
-              />
-            </div>
-          </template>
+            </template>
+          </div>
         </div>
-        <div class="mt-[14px] py-[14px] border-y-[#E6E6E8] border-y-[1px] border-y-solid">
-          <div class="flex flex-col items-end">
+        <div class="mt-[14px] py-[14px] border-y-[#E6E6E8] border-y-0.4 border-y-solid dark:border-y-[rgba(230,230,232,0.3)]">
+          <div class="flex flex-col gap-[10px] items-end">
             <div class="text-[16px] font-semibold text-[#3971F3]">
-              实付金额：0.00
+              <span class="mr-[4px]">实付金额:</span>
+              <span>00.00</span>
             </div>
             <div class="text-[16px] font-semibold text-[#3971F3]">
-              积分合计0.00
+              <span class="mr-[4px]">积分合计:</span>
+              <span>00.00</span>
             </div>
           </div>
         </div>
-        <div class="py-[9px]">
-          <label for="remark">备注信息</label>
+        <div class="flex flex-col gap-[8px] pt-[8px]">
+          <label for="remark color-[#333] dark:color-[#fff]">备注信息</label>
           <textarea
             id="remark" name="remark"
-            class="w-[100%] border-[#CBCDD1] border border-solid rounded-[8px] py-[8px]"
+            class="remark"
+            placeholder="请输入内容"
           />
           <div class="flex">
-            <div class="text-[#3971F3] text-[14px] px-[8px] py-[2px] bg-[#F1F5FE] rounded-[10px]">
+            <div class="text-[#3971F3] text-[14px] px-[8px] bg-[#F1F5FE] rounded-[8px]">
               常用备注
             </div>
           </div>
         </div>
         <div class="text-[#FF2F2F] text-[16px] font-semibold py-[12px]">
-          剩余未支付：0.00
+          <span class="mr-[4px]">剩余未支付:</span>
+          <span>00.00</span>
         </div>
-        <div class="md:grid grid-cols-[1fr_1fr] gap-[16px] lg:grid-cols-[1fr_1fr_1fr]">
-          <div class="flex justify-between">
-            <van-popover v-model:show="showPopover" :actions="actions">
-              <template #reference>
-                <div class="refer">
-                  <div class="row-left">
-                    支付方式
-                  </div>
-                  <div class="row-right">
-                    <van-icon name="arrow-down" />
-                  </div>
+        <div class="grid-cols-[1fr_1fr] gap-[16px]" uno-lg="grid-cols-[1fr_1fr_1fr]" uno-md="grid">
+          <div class="flex flex-col gap-[12px]">
+            <TransitionGroup name="operation">
+              <template v-for="item in items" :key="item">
+                <div class="flex flex-shrink-0 items-center gap-[12px]">
+                  <van-popover v-model:show="item.isPopoverVisible" :actions="item.actions">
+                    <template #reference>
+                      <div class="refer">
+                        <div class="row-left color-[#333] dark:color-[#fff] font-size-[14px]">
+                          支付方式
+                        </div>
+                        <div class="row-right">
+                          <van-icon name="arrow-down" color="#333" />
+                        </div>
+                      </div>
+                    </template>
+                  </van-popover>
+                  <common-frame tip="金额" />
+                  <template v-if="item.id === 1">
+                    <sale-sales-plusminus
+                      @button-click="insertItem()" />
+                  </template>
+                  <template v-else>
+                    <sale-sales-plusminus
+                      :is-add="false"
+                      @button-click="removeItem(item)" />
+                  </template>
                 </div>
               </template>
-            </van-popover>
-            <common-Frame />
+            </TransitionGroup>
           </div>
         </div>
       </div>
@@ -75,6 +111,23 @@ const actions = [
 
 <style lang="scss" scoped>
 .refer {
-  --uno: 'flex-1 px-12px py-6px border-rd-36px text-[14px] flex-between bg-#fff border-[#e6e6e8] border-1px border-solid dark:bg-[rgba(255,255,255,0.2)] dark:border-[rgba(230,230,232,0.2)]';
+  --uno: 'flex-1 gap-[4px] px-12px py-6px border-rd-36px text-[14px] flex-between bg-#fff border-[#e6e6e8] border-1px border-solid dark:bg-[rgba(255,255,255,0.2)] dark:border-[rgba(230,230,232,0.2)]';
+}
+
+.remark {
+  --uno: 'w-auto border-[#CBCDD1] border border-solid rounded-[8px] px-[4px] pt-[4px] pb-[32px] bg-[#fff] tracking-[1px] text-[14px] dark:bg-[rgba(255,255,255,0.2)] dark:border-[rgba(230,230,232,0.3)] color-[#333] dark:color-[#fff] resize-y';
+}
+
+textarea::placeholder {
+  --uno: 'color-#cbcdd1';
+}
+
+.operation-enter-active,
+.operation-leave-active {
+  --uno: 'transition-all ease-in-out duration-500';
+}
+.operation-enter-from,
+.operation-leave-to {
+  --uno: 'translate-x-[30px] opacity-0';
 }
 </style>
--- pages/sale/sales/add.vue
+++ pages/sale/sales/add.vue
@@ -41,7 +41,7 @@ const goodinfo = ref([
       <sale-add-product />
       <sale-add-settlement :goodinfo="goodinfo" />
       <div class="h-[80px]">
-        <common-button-bottom confirm-text="开单并打印" cancel-text="仅开单" />
+        <common-button-bottom confirm-text="开单" cancel-text="取消" />
       </div>
     </div>
   </div>
