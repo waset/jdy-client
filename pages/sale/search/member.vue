6f8f4a5ce6c147f88d7d86201e3a53985f7e9df2
LdddLi <1653243099@qq.com>
2024-10-22 16:31:46 +0800

    feat: 新增会员搜索页、商品搜索页及相关组件; 移除不再使用的页面

---
+++ pages/sale/search/member.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+// 搜索会员页
+useSeoMeta({
+  title: '会员列表',
+})
+
+const memberInfo: MemberInfo = {
+  nickname: '懒羊羊',
+  phone: '19222222222',
+  img: 'https://img.yzcdn.cn/vant/cat.jpeg',
+  email: '16000000000@qq.com',
+  address: '草原地上一层',
+  birthday: '0000-01-01',
+  gender: '未知',
+  remark: '备注信息',
+  createTime: '2021-10-10 10:10:10',
+  level: 3,
+  buyCount: 60,
+  isFollow: 1,
+}
+</script>
+
+<template>
+  <div class="grid-12">
+    <div class="flex flex-col gap-[24px] col-12 pt-[24px]" uno-lg="col-8 offset-2" uno-sm="col-12">
+      <!-- 搜索组件 -->
+      <div class="search-header flex flex-row gap-[12px] px-[16px]">
+        <product-filter-search class="flex-1" />
+        <sale-sales-plusminus />
+      </div>
+      <!-- 文本信息 -->
+      <div class="flex flex-row gap-[4px] items-center color-[#fff] font-size-[14px] px-[16px]">
+        <span>搜索结果共</span>
+        <span>1312321432535</span>
+      </div>
+      <!-- 会员卡片(待删除：如果查到信息，则展示会员卡片，否则展示空状态 ) -->
+      <div class="px-[16px]">
+        <sale-sales-grades :member-info="memberInfo" />
+      </div>
+      <!-- 操作栏 -->
+      <div class="h-[80px]">
+        <common-button-bottom confirm-text="确认" cancel-text="取消" />
+      </div>
+    </div>
+  </div>
+</template>
