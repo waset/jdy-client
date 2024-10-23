e35c06a058784291d9fa39fb91b6b238b93b05df
LdddLi <1653243099@qq.com>
2024-10-23 17:42:28 +0800

    feat: check组件新增圆角属性 ; 结算组件完善选择支付方式的功能

--- components/common/check/Index.vue
+++ components/common/check/Index.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 // 勾选框
+const props = withDefaults(defineProps<{
+  /**
+   * 圆角大小
+   */
+  rounded?: string
+}>(), {
+  rounded: '60px',
+})
 const checked = defineModel<boolean>('checked')
 
 function toggleCheck() {
@@ -8,14 +16,20 @@ function toggleCheck() {
 </script>
 
 <template>
-  <div class="check-box" @click="toggleCheck">
+  <div @click="toggleCheck">
     <template v-if="checked">
-      <div class="flex justify-center items-center rounded-full w-4 h-4 border-solid border-2 border-[#3971F3] bg-[#fff]">
+      <div class="check-box" :style="{ borderRadius: props.rounded }">
         <icon name="jdy:check-box" class="w-full h-full text-[#3971F3]" />
       </div>
     </template>
     <template v-else>
-      <div class="rounded-full w-4 h-4 border-solid border-2 border-[#CBCDD1]" />
+      <div class="w-4 h-4 border-solid border-2 border-[#CBCDD1]" :style="{ borderRadius: props.rounded }" />
     </template>
   </div>
 </template>
+
+<style scoped>
+.check-box {
+  --uno: 'flex justify-center items-center w-4 h-4 border-solid border-2 border-[#3971F3] bg-[#fff]';
+}
+</style>
