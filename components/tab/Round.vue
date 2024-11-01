61c9e64eef25cbd184e28a032e0d6a3007ae0fc5
XieJingao <496929151@qq.com>
2024-11-01 17:54:56 +0800

    feat: 增加圆形和矩形选项卡

---
+++ components/tab/Round.vue
@@ -0,0 +1,39 @@
+<!-- 圆角选项卡 -->
+<script lang="ts" setup>
+const props = defineProps<{
+  navList: string[]
+}>()
+const current = defineModel({ type: Number, default: 0 })
+function changeNav(i: number) {
+  current.value = i
+}
+</script>
+
+<template>
+  <div class="nav">
+    <template v-for="(item, index) in props.navList" :key="index">
+      <div class="item" :class="current === index ? 'selected' : 'unselect'" @click="changeNav(index)">
+        {{ item }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav {
+  border: 1px #2c86fe solid;
+  --uno: 'flex w-fit h-fit mx-auto rounded-full overflow-hidden';
+  .item {
+    --uno: 'text-14px text-center py-8px px-16px bg-[#fff] shrink-0';
+    &:not(:last-child) {
+      border-right: 1px solid #2c86fe;
+    }
+  }
+  .unselect {
+    --uno: 'text-#8C8C8C';
+  }
+  .selected {
+    --uno: 'bg-#2c86fe text-#fff';
+  }
+}
+</style>
